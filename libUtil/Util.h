4a30b878b8261427f6cfb69bed12e009e2971219
Andrei Formiga <archimedes_siracusa@hotmail.com>
2013-04-26 01:04:22 -0300

    Melhorias na formatacao do codigo.

--- libUtil/Util.h
+++ libUtil/Util.h
@@ -5,31 +5,31 @@
 #include <string.h>
 
 class Util{
-	private:
-		MTRand *mtRand;
-				
-	public:
-		Util();
-		virtual ~Util();
+ private:
+    MTRand *mtRand;
 
-		//static void setMtRand(MTRand *mtRand2);
-		static double dRand();
-		
-		static void copy(double *xAux, double *x, int n);
-		static double calcNorma(double *x, double *xGrid, int n);
-		static double calcNorma(double *x, int n);
-		static double dist(double *x1, double *x2, int n);		
+ public:
+    Util();
+    virtual ~Util();
 
-		static bool feasible(double *x, double *l, double *u, int n);
-		static bool equals(double x1, double x2);
-		static void printX(double *x, int n);
+    //static void setMtRand(MTRand *mtRand2);
+    static double dRand();
 
-		static double maxreal(double x1, double x2);
-		static double minreal(double x1, double x2);
-		static double dotproduct(double *x1, double *x2, int n);
-		static double sqr(double x1);
-		static void addvector(double *x1, double *x2, int n);
-		static void subvector(double *x1, double *x2, int n);
+    static void copy(double *xAux, double *x, int n);
+    static double calcNorma(double *x, double *xGrid, int n);
+    static double calcNorma(double *x, int n);
+    static double dist(double *x1, double *x2, int n);
+
+    static bool feasible(double *x, double *l, double *u, int n);
+    static bool equals(double x1, double x2);
+    static void printX(double *x, int n);
+
+    static double maxreal(double x1, double x2);
+    static double minreal(double x1, double x2);
+    static double dotproduct(double *x1, double *x2, int n);
+    static double sqr(double x1);
+    static void addvector(double *x1, double *x2, int n);
+    static void subvector(double *x1, double *x2, int n);
 
 
 };
